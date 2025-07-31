9d5a3e5114033b4dc24d94d664799ee4ab7431ea
rlyumanov <rlyumanov@gmail.com>
2025-07-31 13:51:46 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="data-utils",
+    name="data_utils",
     version="0.1.0",
     author="Your Name",
     author_email="rlyumanov@gmail.com",
